cd8e8fa3ca733d77a9a57f01bc83d9d6cc14535c
Ed Stott <ed.stott@ic.ac.uk>
2021-06-11 13:27:01 -0700

    Remove bad lines in Verilog. Add error message for synthesis failed

--- issie-synth.sh
+++ issie-synth.sh
@@ -7,11 +7,30 @@ fi
 
 cd $1
 
+#Remove "q = 0;" lines from source files
+for VFILE in *.v
+do
+	grep -v 'q = 0;' "${VFILE}" > "${VFILE}_"
+	mv "${VFILE}_" "${VFILE}"
+done
+
+#Synthesis
 echo "Running Yosys with source files:"
 ls ./*.v
+rm main.json
 eval "yosys -p 'synth_ice40 -top main -json main.json' ./*.v &> main.log"
+
+if [ ! -f main.json ] ; then
+	echo "Synthesis Failed"
+	exit
+fi
+
+echo "Synthesis Complete"
+
+#Place and route
 echo "Running nextpnr"
-eval "nextpnr-ice40 --hx4k --json main.json --asc main.asc &>> main.log"
+eval "nextpnr-ice40 --hx8k --json main.json --asc main.asc &>> main.log"
+echo "Generated log $1/main.log"
 
 exit 0
 
